7a60cefa6bfafd7cbf38f495a7a2604f82da1dd0
travis martin <travismartin266@gmail.com>
2021-09-27 08:45:32 -0300

    adding priorityqueue string testing sorting

--- app/src/main/java/datastructure/algorithms/heaps/Main.java
+++ app/src/main/java/datastructure/algorithms/heaps/Main.java
@@ -16,27 +16,40 @@ public class Main {
     // Multi threaded use the PriorityBlockingQueue class
 
     public static void main(String[] args) {
-        Heap heap = new Heap(10);
-
-        heap.insert(80);
-        heap.insert(75);
-        heap.insert(60);
-        heap.insert(68);
-        heap.insert(55);
-        heap.insert(40);
-        heap.insert(52);
-        heap.insert(67);
-
-        heap.printHeap();
-        heap.sort();
-        heap.printHeap();
+//        Heap heap = new Heap(10);
+//
+//        heap.insert(80);
+//        heap.insert(75);
+//        heap.insert(60);
+//        heap.insert(68);
+//        heap.insert(55);
+//        heap.insert(40);
+//        heap.insert(52);
+//        heap.insert(67);
+//
+//        heap.printHeap();
+//        heap.sort();
+//        heap.printHeap();
 //        heap.printHeap();
 //        System.out.println(heap.peek());
 //        heap.delete(0);
 //        System.out.println(heap.peek());
 
+        String a = "good";
+        String out = a.substring(0,1);
+        System.out.println(out);
 
-//        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<String> pq = new PriorityQueue<>();
+        pq.add("aba");
+        pq.add("aca");
+        pq.add("aaa");
+        pq.add("cca");
+        pq.add("aba");
+        System.out.println(pq.size());
+//
+//        System.out.println(pq.peek());
+//        System.out.println(pq.poll());
+//        System.out.println(pq.peek());
 //        pq.add(25);
 //        pq.add(-22);
 //        pq.add(1343);
